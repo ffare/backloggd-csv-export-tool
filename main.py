5d85134560568ba20ca70ed7db9d3d85f8b0300b
ffare <faresbenlakdher@gmail.com>
2024-03-19 13:05:22 +0100

    find keywords and add them to list

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import requests
 
-x = requests.get('https://www.backloggd.com/u/Hollow/wishlist/')
-print(x.text)
+with open('debug/result.html', 'w') as file:
+    x = requests.get('https://www.backloggd.com/u/Hollow/wishlist/')
+    file.write(x.text)
+
+link_list = []
+game_name_list = []
+link_keyword = '<a href="/games/'
+game_name_keyword = '<div class="game-text-centered"'
+for line in x.text.splitlines():
+    if link_keyword in line:
+        link_list.append(line.strip())
+        
+print(link_list)
