c6fc6a820e7e5305cffbee26963014bf46c52b78
ffare <faresbenlakdher@gmail.com>
2024-03-19 14:29:34 +0100

    using regex for filtering instead)

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import requests
+import re
 
 with open('debug/result.html', 'w') as file:
     x = requests.get('https://www.backloggd.com/u/Hollow/wishlist/')
     file.write(x.text)
 
-link_list = []
-game_name_list = []
 link_keyword = '<a href="/games/'
 game_name_keyword = '<div class="game-text-centered"'
-for line in x.text.splitlines():
-    if link_keyword in line:
-        link_list.append(line.strip())
-        
+
+link_list = []
+game_name_list = []
+
+list = re.findall(r'<a href="/games/.*', x.text)
+for w in list:
+    matches = re.search(r'games/[^/]+/', w)
+    if matches:
+        link_list.append(matches.group())
+
+    
 print(link_list)
