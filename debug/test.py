82bfb7f6161ccfda06b2ca12f7cce455270e80dd
ffare <faresbenlakdher@gmail.com>
2024-03-21 23:07:09 +0100

    added csv export

--- debug/test.py
+++ debug/test.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import csv
 
 def getListFromKeyword(text, keyword, flags=0):
     return re.findall(keyword, text, flags)
@@ -7,14 +8,15 @@ def getListFromKeyword(text, keyword, flags=0):
 def getGameName(text):
     list = getListFromKeyword(text, r'<div class="col-auto pr-1">(.*?)</div>', flags=re.DOTALL)
     new_list = re.findall(r'<h1 class="mb-0">(.*?)</h1>', list[0], flags=re.DOTALL)
-    return new_list
+    return new_list[0]
 
-with open('result.html', 'w', encoding='utf-8') as file:
+with open('result.html', 'w', encoding='utf-8') as file, open('example.csv', 'w', newline='') as csvf:
     x = requests.get('https://www.backloggd.com/games/heroes-of-might-and-magic-ii-gold/')
+    writer = csv.writer(csvf, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
     if (x.status_code == 200):
         file.write(x.text)
-        
-print(getGameName(x.text))  
+        writer.writerow((getGameName(x.text), 'test1', 'test2', ['1', '2']))
+    
 
 
 #print(list)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 import re
 import time
+import csv
 
 username = 'Hollow'
 
@@ -21,10 +22,6 @@ def getMaxNumberofPages(user, text):
 def getReleaseDate(text):
     list = getListFromKeyword(text, r'<div class="col-auto mt-auto pr-0">\n.*')
     return list[0].split('>')[5][:-3]
-    
-def getGameName(text):
-    list = getListFromKeyword(text, r'<div class="col-12 pr-0">\n.*\n.*\n.*')
-    return list[0].split('<h1 class="mb-0">')[1][:-5]
 
 def getCompanyNames(text):    
     list = getListFromKeyword(text, r'<div class="col-auto pl-lg-1 sub-title">(.*?)</div>', flags=re.DOTALL)
@@ -40,37 +37,29 @@ def getGameName(text):
     else:
         return re.findall(r'<h1 class="mb-0">(.*?)</h1>', list[0], flags=re.DOTALL)[0]
 
-with open('debug/result.html', 'w') as file:
+with open('debug/result.html', 'w') as file, open('import/import.csv', 'w', newline='') as csvf:
     x = requests.get('https://www.backloggd.com/u/'+username+'/wishlist/')
     file.write(x.text)
-
-link_list = []
-gameName_list = []
-maxpages = getMaxNumberofPages(username, x.text)
-for i in range(maxpages):
-    x = requests.get('https://www.backloggd.com/u/'+username+'/wishlist/?page='+str(i+1))
+    writer = csv.writer(csvf, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)    
     
-    list = getListFromKeyword(x.text, r'<a href="/games/[^/]+/')
-    # Fetch information after accessing links
-    for w in list:
-        w = w[10:]       
-        link_list.append(w)
-                       
-        print('https://www.backloggd.com/'+str(w))
+    link_list = []
+    gameName_list = []
+    maxpages = getMaxNumberofPages(username, x.text)
+    for i in range(maxpages):
+        x = requests.get('https://www.backloggd.com/u/'+username+'/wishlist/?page='+str(i+1))
         
-        s_time = time.time()
-        y = requests.get('https://www.backloggd.com/'+str(w))
-        print("elapsed time: "+str(time.time() - s_time)[:4])
-        
-        print(getGameName(y.text))
-        print(getReleaseDate(y.text))
-        print(getCompanyNames(y.text))
-        print('\n')
-
-date_list = []
-for link in link_list:
-    x = requests.get('https://www.backloggd.com/games/'+str(link)+'/')
-    if (x.status_code==200):
-        date_list.append(getReleaseDate(x.text))
-        
-print(date_list)
+        list = getListFromKeyword(x.text, r'<a href="/games/[^/]+/')
+        # Fetch information after accessing links
+        for w in list:
+            w = w[10:]       
+            link_list.append(w)
+                           
+            print('https://www.backloggd.com/'+str(w))
+            
+            s_time = time.time()
+            y = requests.get('https://www.backloggd.com/'+str(w))
+            print("elapsed time: "+str(time.time() - s_time)[:4])
+            
+            writer.writerow((getGameName(y.text), getReleaseDate(y.text), getCompanyNames(y.text)))
+            print((getGameName(y.text), getReleaseDate(y.text), getCompanyNames(y.text)))
+            print('\n')
