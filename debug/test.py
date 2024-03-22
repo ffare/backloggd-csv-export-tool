6ebf80c3fa2161a8b6a993b4d24c373d95d0331f
ffare <faresbenlakdher@gmail.com>
2024-03-22 16:29:32 +0100

    added threading to handle get requests

--- debug/test.py
+++ debug/test.py
@@ -1,6 +1,9 @@
 import requests
 import re
 import csv
+import threading
+import concurrent.futures
+import time
 
 def getListFromKeyword(text, keyword, flags=0):
     return re.findall(keyword, text, flags)
@@ -10,13 +13,14 @@ def getGameName(text):
     new_list = re.findall(r'<h1 class="mb-0">(.*?)</h1>', list[0], flags=re.DOTALL)
     return new_list[0]
 
-with open('result.html', 'w', encoding='utf-8') as file, open('example.csv', 'w', newline='') as csvf:
-    x = requests.get('https://www.backloggd.com/games/heroes-of-might-and-magic-ii-gold/')
-    writer = csv.writer(csvf, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-    if (x.status_code == 200):
-        file.write(x.text)
-        writer.writerow((getGameName(x.text), 'test1', 'test2', ['1', '2']))
+def printtest():
+    t = time.time()
+    print("working")
+    print(time.time() - t)
     
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+while True:
+    pool.submit(printtest())
 
 
 #print(list)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 import re
 import time
 import csv
+import concurrent.futures
 
 username = 'Hollow'
 
@@ -26,9 +27,10 @@ def getReleaseDate(text):
 def getCompanyNames(text):    
     list = getListFromKeyword(text, r'<div class="col-auto pl-lg-1 sub-title">(.*?)</div>', flags=re.DOTALL)
     if not list:
-        return ['TDB']
+        return 'TDB'
     else:
-        return re.findall(r'<a href="/company/.*?>(.*?)</a>', list[0], flags=re.DOTALL)
+        final_list = re.findall(r'<a href="/company/.*?>(.*?)</a>', list[0], flags=re.DOTALL)
+        return ', '.join(final_list)
 
 def getGameName(text):
     list = getListFromKeyword(text, r'<div class="col-auto pr-1">(.*?)</div>', flags=re.DOTALL)
@@ -37,29 +39,30 @@ def getGameName(text):
     else:
         return re.findall(r'<h1 class="mb-0">(.*?)</h1>', list[0], flags=re.DOTALL)[0]
 
-with open('debug/result.html', 'w') as file, open('import/import.csv', 'w', newline='') as csvf:
+def getRequest(text):
+    return requests.get(text, timeout=5)
+
+with open('debug/result.html', 'w', encoding='utf-8') as file, open('export/export.csv', 'w', newline='') as csvf:
     x = requests.get('https://www.backloggd.com/u/'+username+'/wishlist/')
     file.write(x.text)
-    writer = csv.writer(csvf, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)    
+    writer = csv.writer(csvf, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+    writer.writerow(['Name', 'Release Date', 'Companies'])  # Write header 
     
     link_list = []
-    gameName_list = []
     maxpages = getMaxNumberofPages(username, x.text)
     for i in range(maxpages):
         x = requests.get('https://www.backloggd.com/u/'+username+'/wishlist/?page='+str(i+1))
         
         list = getListFromKeyword(x.text, r'<a href="/games/[^/]+/')
-        # Fetch information after accessing links
-        for w in list:
-            w = w[10:]       
-            link_list.append(w)
-                           
-            print('https://www.backloggd.com/'+str(w))
-            
-            s_time = time.time()
-            y = requests.get('https://www.backloggd.com/'+str(w))
-            print("elapsed time: "+str(time.time() - s_time)[:4])
-            
-            writer.writerow((getGameName(y.text), getReleaseDate(y.text), getCompanyNames(y.text)))
-            print((getGameName(y.text), getReleaseDate(y.text), getCompanyNames(y.text)))
-            print('\n')
+        list = [w[10:] for w in list]
+        
+        # Fetch information after accessing links        
+        with concurrent.futures.ThreadPoolExecutor(max_workers=len(list)) as executor:
+            results = [executor.submit(getRequest, 'https://www.backloggd.com/'+str(w)) for w in list]
+        
+            for future in concurrent.futures.as_completed(results):
+                y = future.result()
+                    
+                writer.writerow((getGameName(y.text), getReleaseDate(y.text), getCompanyNames(y.text)))
+                print((getGameName(y.text), getReleaseDate(y.text), getCompanyNames(y.text)))
+                print('\n')
